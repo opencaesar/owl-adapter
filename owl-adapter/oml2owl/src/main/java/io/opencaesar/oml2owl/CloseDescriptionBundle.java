bd3df5aa958c661d647a48c0abda399bee15a500
Steven Jenkins <j.s.jenkins@jpl.nasa.gov>
2020-08-26 10:24:59 -0700

    work in progress

--- owl-adapter/oml2owl/src/main/java/io/opencaesar/oml2owl/CloseDescriptionBundle.java
+++ owl-adapter/oml2owl/src/main/java/io/opencaesar/oml2owl/CloseDescriptionBundle.java
@@ -1,9 +1,22 @@
 package io.opencaesar.oml2owl;
 
+import static io.opencaesar.oml.util.OmlRead.getImportedOntologies;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
 import org.eclipse.emf.ecore.resource.Resource;
+import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLOntology;
 
 import io.opencaesar.closeworld.OwlApi;
+import io.opencaesar.closeworld.Taxonomy;
+import io.opencaesar.oml.Ontology;
+import io.opencaesar.oml.util.OmlRead;
 
 public class CloseDescriptionBundle {
 
@@ -13,6 +26,20 @@ public class CloseDescriptionBundle {
 		this.resource = resource;
 	}
 
+	private <T> Stream<T> toStream(Iterator<T> i) {
+		return StreamSupport.stream(Spliterators.spliteratorUnknownSize(i, Spliterator.ORDERED), false);
+	}
+	
+	private static HashMap<IRI, HashMap<IRI, Integer>> dataPropertyCounts(final Iterable<Ontology> allOntologies) {
+		final HashMap<IRI, HashMap<IRI, Integer>> map = new HashMap<>();
+		return map;
+	}
+	
+	private static HashMap<IRI, HashMap<IRI, Integer>> objectPropertyCounts(final Iterable<Ontology> allOntologies) {
+		final HashMap<IRI, HashMap<IRI, Integer>> map = new HashMap<>();
+		return map;
+	}
+	
 	public static class CloseDescriptionBundleToOwl extends CloseDescriptionBundle {
 		protected final OWLOntology ontology;
 		protected final OwlApi owlApi;
@@ -24,6 +51,10 @@ public class CloseDescriptionBundle {
 		}
 
 		public void run() {
+			final Ontology omlOntology = OmlRead.getOntology(this.resource);
+			final Iterable<Ontology> allOntologies = OmlRead.reflexiveClosure(omlOntology, o -> getImportedOntologies(o));
+			final HashMap<IRI, HashMap<IRI, Integer>> dataPropertyCounts = dataPropertyCounts(allOntologies);
+			final HashMap<IRI, HashMap<IRI, Integer>> objectPropertyCounts = objectPropertyCounts(allOntologies);
 		}
 	}
 
