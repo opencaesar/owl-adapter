b4d86942b0c051d5607a5f4fa1bad165f1b5eddf
Steven Jenkins <j.s.jenkins@jpl.nasa.gov>
2020-08-26 21:41:28 -0700

    works for forward properties

--- owl-adapter/oml2owl/src/main/java/io/opencaesar/oml2owl/CloseDescriptionBundle.java
+++ owl-adapter/oml2owl/src/main/java/io/opencaesar/oml2owl/CloseDescriptionBundle.java
@@ -1,7 +1,5 @@
 package io.opencaesar.oml2owl;
 
-import static io.opencaesar.oml.util.OmlRead.getImportedOntologies;
-
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Spliterator;
@@ -11,13 +9,19 @@ import java.util.stream.StreamSupport;
 
 import org.eclipse.emf.ecore.resource.Resource;
 import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLClassAssertionAxiom;
+import org.semanticweb.owlapi.model.OWLDataMaxCardinality;
+import org.semanticweb.owlapi.model.OWLDataProperty;
+import org.semanticweb.owlapi.model.OWLIndividual;
+import org.semanticweb.owlapi.model.OWLNamedIndividual;
+import org.semanticweb.owlapi.model.OWLObjectMaxCardinality;
+import org.semanticweb.owlapi.model.OWLObjectProperty;
 import org.semanticweb.owlapi.model.OWLOntology;
 
 import io.opencaesar.closeworld.OwlApi;
-import io.opencaesar.oml.IdentifiedElement;
-import io.opencaesar.oml.LinkAssertion;
 import io.opencaesar.oml.NamedInstance;
 import io.opencaesar.oml.Ontology;
+import io.opencaesar.oml.Relation;
 import io.opencaesar.oml.ScalarProperty;
 import io.opencaesar.oml.ScalarPropertyValueAssertion;
 import io.opencaesar.oml.StructuredProperty;
@@ -40,25 +44,11 @@ public class CloseDescriptionBundle {
 	private static HashMap<String, HashMap<String, Integer>> dataPropertyCounts(final Iterable<Ontology> allOntologies) {
 		final HashMap<String, HashMap<String, Integer>> map = new HashMap<>();
 		toStream(allOntologies.iterator()).forEach(g -> {
-			toStream(g.eAllContents()).filter(e -> e instanceof ScalarPropertyValueAssertion).map(a -> (ScalarPropertyValueAssertion) a).forEach(assertion -> {
-				final HashMap<String, Integer> subj_map = map.getOrDefault(OmlRead.getIri((IdentifiedElement) assertion.getOwningInstance()), new HashMap<String, Integer>());
-				final String prop_iri = OmlRead.getIri(assertion.getProperty());
-				final Integer count = subj_map.getOrDefault(prop_iri, 0);
-				subj_map.put(prop_iri, count + 1);
-			});
-		});
-
-		return map;
-	}
-	
-	@SuppressWarnings("boxing")
-	private static HashMap<String, HashMap<String, Integer>> objectPropertyCounts(final Iterable<Ontology> allOntologies) {
-		final HashMap<String, HashMap<String, Integer>> map = new HashMap<>();
-		toStream(allOntologies.iterator()).forEach(g -> {
 			toStream(g.eAllContents()).filter(e -> e instanceof NamedInstance).forEach(e -> {
 				final NamedInstance subj = (NamedInstance) e;
 				final String subj_iri = OmlRead.getIri(subj);
 				final HashMap<String, Integer> subj_map = map.getOrDefault(subj_iri, new HashMap<String, Integer>());
+				map.put(subj_iri, subj_map);
 				subj.getOwnedPropertyValues().forEach(pva -> {
 					if (pva instanceof ScalarPropertyValueAssertion) {
 						final ScalarPropertyValueAssertion spva = (ScalarPropertyValueAssertion) pva;
@@ -80,6 +70,27 @@ public class CloseDescriptionBundle {
 		return map;
 	}
 	
+	@SuppressWarnings("boxing")
+	private static HashMap<String, HashMap<String, Integer>> objectPropertyCounts(final Iterable<Ontology> allOntologies) {
+		final HashMap<String, HashMap<String, Integer>> map = new HashMap<>();
+		toStream(allOntologies.iterator()).forEach(g -> {
+			toStream(g.eAllContents()).filter(e -> e instanceof NamedInstance).forEach(e -> {
+				final NamedInstance subj = (NamedInstance) e;
+				final String subj_iri = OmlRead.getIri(subj);
+				final HashMap<String, Integer> subj_map = map.getOrDefault(subj_iri, new HashMap<String, Integer>());
+				map.put(subj_iri,  subj_map);
+				subj.getOwnedLinks().forEach(la -> {
+					final Relation rel = la.getRelation();
+					final String rel_iri = OmlRead.getIri(rel);
+					final Integer count = subj_map.getOrDefault(rel_iri, 0);
+					subj_map.put(rel_iri, count + 1);
+				});
+			});
+		});
+
+		return map;
+	}
+	
 	public static class CloseDescriptionBundleToOwl extends CloseDescriptionBundle {
 		protected final OWLOntology ontology;
 		protected final OwlApi owlApi;
@@ -90,16 +101,29 @@ public class CloseDescriptionBundle {
 			this.owlApi = owlApi;
 		}
 
+		@SuppressWarnings("boxing")
 		public void run() {
 			final Ontology omlOntology = OmlRead.getOntology(this.resource);
-			final Iterable<Ontology> allOntologies = OmlRead.reflexiveClosure(omlOntology, o -> getImportedOntologies(o));
+			final Iterable<Ontology> allOntologies = OmlRead.reflexiveClosure(omlOntology, o -> OmlRead.getImportedOntologies(o));
 			final HashMap<String, HashMap<String, Integer>> dataPropertyCounts = dataPropertyCounts(allOntologies);
 			final HashMap<String, HashMap<String, Integer>> objectPropertyCounts = objectPropertyCounts(allOntologies);
 			
 			dataPropertyCounts.forEach((subj, map) -> {
-				final IRI subj_iri = IRI.create(subj);
-				map.forEach((prop, count) -> {
-					final IRI prop_iri = IRI.create(prop);
+				final OWLNamedIndividual ni = this.owlApi.getOWLNamedIndividual(IRI.create(subj));
+				map.forEach((prop, c) -> {
+					final OWLDataProperty dp = this.owlApi.getOWLDataProperty(IRI.create(prop));
+					final OWLDataMaxCardinality mc = this.owlApi.getOWLDataMaxCardinality(c, dp);
+					final OWLClassAssertionAxiom ca = this.owlApi.getOWLClassAssertionAxiom(mc, ni);
+					this.ontology.add(ca);
+				});
+			});
+			objectPropertyCounts.forEach((subj, map) -> {
+				final OWLNamedIndividual ni = this.owlApi.getOWLNamedIndividual(IRI.create(subj));
+				map.forEach((prop, c) -> {
+					final OWLObjectProperty dp = this.owlApi.getOWLObjectProperty(IRI.create(prop));
+					final OWLObjectMaxCardinality mc = this.owlApi.getOWLObjectMaxCardinality(c, dp);
+					final OWLClassAssertionAxiom ca = this.owlApi.getOWLClassAssertionAxiom(mc, ni);
+					this.ontology.add(ca);
 				});
 			});
 		}
